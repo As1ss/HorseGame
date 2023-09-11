e99c54443993b517894464e67bb54e1e13018d85
As1ss <alexis.lopbri@educa.jcyl.es>
2023-09-11 17:48:25 +0200

    Añadido de posición aleatoria de la figura del caballo sobre el tablero y pintado de la posición anterior

--- app/src/main/java/com/example/horsegame/MainActivity.kt
+++ app/src/main/java/com/example/horsegame/MainActivity.kt
@@ -8,14 +8,55 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TableRow
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+    private var cellSelected_x = 0
+    private var cellSelected_y = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
         initScreenGame()
+        setFirstPosition()
+    }
+
+    private fun setFirstPosition() {
+        var x = 0
+        var y = 0
+        x = (0..7).random()
+        y = (0..7).random()
+        cellSelected_x = x
+        cellSelected_y = y
+
+        selectCell(x, y)
+    }
+
+    private fun selectCell(x: Int, y: Int) {
+
+
+        paintHorseCell(cellSelected_x, cellSelected_y, "previous_cell")
+
+        cellSelected_x = x
+        cellSelected_y = y
+
+        paintHorseCell(x, y, "selected_cell")
+
+
+    }
+
+    private fun paintHorseCell(x: Int, y: Int, color: String) {
+        var iv: ImageView = findViewById(resources.getIdentifier("c$x$y", "id", packageName))
+        iv.setBackgroundColor(
+            ContextCompat.getColor(
+                this,
+                resources.getIdentifier(color, "color", packageName)
+            )
+        )
+        iv.setImageResource(R.drawable.horse)
+
+
     }
 
     private fun initScreenGame() {
@@ -34,20 +75,28 @@ class MainActivity : AppCompatActivity() {
         val display = windowManager.defaultDisplay
         val size = Point()
         display.getSize(size)
-        val width= size.x
+        val width = size.x
 
         var width_dp = (width / resources.displayMetrics.density)
         var lateralMarginDP = 0
-        val width_cell = (width_dp - lateralMarginDP)/8
+        val width_cell = (width_dp - lateralMarginDP) / 8
         val height_cell = width_cell
-        for (i in 0..7){
-            for (j in 0..7){
-                iv = findViewById(resources.getIdentifier("c$i$j", "id",packageName))
+        for (i in 0..7) {
+            for (j in 0..7) {
+                iv = findViewById(resources.getIdentifier("c$i$j", "id", packageName))
 
-                var height = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,height_cell,resources.displayMetrics).toInt()
-                var width = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,width_cell,resources.displayMetrics).toInt()
+                var height = TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP,
+                    height_cell,
+                    resources.displayMetrics
+                ).toInt()
+                var width = TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP,
+                    width_cell,
+                    resources.displayMetrics
+                ).toInt()
 
-                iv.setLayoutParams(TableRow.LayoutParams(width,height))
+                iv.setLayoutParams(TableRow.LayoutParams(width, height))
             }
         }
     }
