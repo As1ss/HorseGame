8a3e5ead5ff52eb9acd1e3b4d94d2b866ea8da3c
As1ss <alexis.lopbri@educa.jcyl.es>
2023-09-12 20:02:50 +0200

    Realizacion de la matriz del tablero de juego y limitaciones de movimientos del usuario

--- app/src/main/java/com/example/horsegame/MainActivity.kt
+++ app/src/main/java/com/example/horsegame/MainActivity.kt
@@ -13,13 +13,76 @@ import androidx.core.content.ContextCompat
 class MainActivity : AppCompatActivity() {
     private var cellSelected_x = 0
     private var cellSelected_y = 0
+
+    private lateinit var board: Array<IntArray>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
         initScreenGame()
+        resetBoard()
         setFirstPosition()
+    }
+
+    private fun resetBoard() {
+        // 0 esta libre
+        // 1 casilla marcada
+        // 2 es un bonus
+        // 9 es una opción del movimiento actual
+
+        board = arrayOf(
+            intArrayOf(0, 0, 0, 0, 0, 0, 0, 0),
+            intArrayOf(0, 0, 0, 0, 0, 0, 0, 0),
+            intArrayOf(0, 0, 0, 0, 0, 0, 0, 0),
+            intArrayOf(0, 0, 0, 0, 0, 0, 0, 0),
+            intArrayOf(0, 0, 0, 0, 0, 0, 0, 0),
+            intArrayOf(0, 0, 0, 0, 0, 0, 0, 0),
+            intArrayOf(0, 0, 0, 0, 0, 0, 0, 0),
+            intArrayOf(0, 0, 0, 0, 0, 0, 0, 0),
+        )
+
+
+        /* Esta forma sería la más óptima pero vamos a rellenar todas las casillas
+            a mano para poder visualizar mejor el tablero.
+         for (i in 0 until board.size) {
+            for (j in 0 until board[i].size) {
+                board[i][j] = 0
+            }
+        }
+         */
+
+    }
+
+
+    fun checkCellClicked(v: View) {
+        var name = v.tag.toString()
+        var x = name.subSequence(1, 2).toString().toInt()
+        var y = name.subSequence(2, 3).toString().toInt()
+
+        checkCell(x, y)
+    }
+
+    private fun checkCell(x: Int, y: Int) {
+        var dif_x = x - cellSelected_x
+        var dif_y = y - cellSelected_y
+
+        var checkTrue = false
+
+        if (dif_x == 1 && dif_y == 2) checkTrue = true //right - top long
+        if (dif_x == 1 && dif_y == -2) checkTrue = true //right - bottom long
+        if (dif_x == 2 && dif_y == 1) checkTrue = true //right long - top
+        if (dif_x == 2 && dif_y == -1) checkTrue = true //right long - top
+        if (dif_x == -1 && dif_y == 2) checkTrue = true //left - top long
+        if (dif_x == -1 && dif_y == -2) checkTrue = true //left - bottom long
+        if (dif_x == -2 && dif_y == 1) checkTrue = true //left long - top
+        if (dif_x == -2 && dif_y == -1) checkTrue = true //left long - bottom
+
+        if (board[x][y] == 1) checkTrue = false
+
+        if (checkTrue) selectCell(x, y)
+
+
     }
 
     private fun setFirstPosition() {
@@ -35,6 +98,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun selectCell(x: Int, y: Int) {
 
+        board[x][y] = 1
 
         paintHorseCell(cellSelected_x, cellSelected_y, "previous_cell")
 
